45db832e390ea411bd40b32e225dd5f38020fe49
Ruben Maloba <rubennkulu09@gmail.com>
2026-02-13 17:27:30 -0500

    chore(lhci): use desktop preset for Lighthouse view

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,6 +4,9 @@ module.exports = {
       startServerCommand:
         "npm run preview -- --host 127.0.0.1 --port 3000 --strictPort",
       numberOfRuns: 1,
+      settings: {
+        preset: "desktop",
+      },
       url: [
         "http://localhost:3000/",
         "http://localhost:3000/contact",
